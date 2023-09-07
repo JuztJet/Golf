043d4e50d8498773c6bbffeeb16a8369b182263e
JuztJet <104680705+JuztJet@users.noreply.github.com>
2023-09-07 13:46:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+#Physics engine
+
 import pygame
+import math
+
+
+pygame.init()  #Starting Pygame
+w,h = 1000, 700  #Getting width height
+
+screen = pygame.display.set_mode((w,h)) #Main widnows called screen, width,height is w,h
+screen.fill('White')
+running = True
+
+#Variable for physics
+
+grav = -3.5
+initial_vel = 40
+angle = 45
+time = 0
+x_pos, w_pos = w*.2, h*0.8
+
+circle = pygame.draw.circle(screen, "Blue", (w/2, h/0.8), 20)
+
+
+#Pygame Loop
+
+while running:
+  time +=0.5
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+  pygame.draw.circle(screen, "Blue", (x_pos, w_pos), 20)
+  x_pos = 2*time
+  print(x_pos)
+
+  
+
+
+  pygame.display.flip()
